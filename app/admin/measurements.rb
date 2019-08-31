ffa3dccd4a49fc75a15c2e4135a4a38e02aa15c7
Raghu Betina <raghu@firstdraft.com>
2019-08-31 16:43:02 +0000

    Whitelist columns in Measurement for ActiveAdmin

--- app/admin/measurements.rb
+++ app/admin/measurements.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Measurement do
+
+ permit_params :shirt_size, :pant_size, :shoe_size, :instagram_link, :image_link
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
